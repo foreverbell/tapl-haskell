4ecceec92d4d91819108fb4f23becb7609deed84
foreverbell <dql.foreverbell@gmail.com>
2016-06-02 14:27:41 +0800

    refactor

--- tapl/simplebool/Evaluator.hs
+++ tapl/simplebool/Evaluator.hs
@@ -2,8 +2,35 @@ module Evaluator (
   evaluate
 ) where
 
-import DeBruijn (shift, substitute)
 import Base
+
+-- | Shift up deBruijn indices of all free variables by delta.
+shift :: Term -> Int -> Term
+shift t delta = go 0 t
+  where
+    go :: Int -> Term -> Term
+    go cutoff (TermVar var)
+      | var >= cutoff = TermVar (var + delta)
+      | otherwise = TermVar var
+    go cutoff (TermAbs var ty t) = TermAbs var ty (go (cutoff + 1) t)
+    go cutoff (TermApp t1 t2) = TermApp (go cutoff t1) (go cutoff t2)
+    go cutoff (TermIfThenElse t1 t2 t3) = TermIfThenElse (go cutoff t1) (go cutoff t2) (go cutoff t3)
+    go _ TermTrue = TermTrue
+    go _ TermFalse = TermFalse
+
+-- | Substitute the variable with 0 deBruijn index in term to subterm.
+substitute :: Term -> Term -> Term
+substitute t subt = go 0 subt t
+  where
+    go :: Int -> Term -> Term -> Term
+    go index sub (TermVar var)
+      | var == index = sub
+      | otherwise = TermVar var
+    go index subt (TermAbs var ty t) = TermAbs var ty (go (index + 1) (shift subt 1) t)
+    go index subt (TermApp t1 t2) = TermApp (go index subt t1) (go index subt t2)
+    go index subt (TermIfThenElse t1 t2 t3) = TermIfThenElse (go index subt t1) (go index subt t2) (go index subt t3)
+    go _ _ TermTrue = TermTrue
+    go _ _ TermFalse = TermFalse
 
 isValue :: Term -> Bool
 isValue TermTrue = True
--- tapl/untyped/Evaluator.hs
+++ tapl/untyped/Evaluator.hs
@@ -2,8 +2,29 @@ module Evaluator (
   evaluate
 ) where
 
-import DeBruijn (shift, substitute)
 import Base
+
+-- | Shift up deBruijn indices of all free variables by delta.
+shift :: Term -> Int -> Term
+shift t delta = go 0 t
+  where
+    go :: Int -> Term -> Term
+    go cutoff (TermVar var)
+      | var >= cutoff = TermVar (var + delta)
+      | otherwise = TermVar var
+    go cutoff (TermAbs var t) = TermAbs var (go (cutoff + 1) t)
+    go cutoff (TermApp t1 t2) = TermApp (go cutoff t1) (go cutoff t2)
+
+-- | Substitute the variable with 0 deBruijn index in term to subterm.
+substitute :: Term -> Term -> Term
+substitute t subt = go 0 subt t
+  where
+    go :: Int -> Term -> Term -> Term
+    go index subt (TermVar var)
+      | var == index = subt
+      | otherwise = TermVar var
+    go index subt (TermAbs var t) = TermAbs var (go (index + 1) (shift subt 1) t)
+    go index subt (TermApp t1 t2) = TermApp (go index subt t1) (go index subt t2)
 
 -- | Call-by-value evalutation strategy, treat abstraction term as value.
 -- | A redex is reducible only if its right-hand is evaluated to value.
